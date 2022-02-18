24072ce95f1c2346ea31aa1a7c534d2d228b11bd
Max <max.franck@iki.fi>
2022-02-18 22:19:47 +0200

    Removed all contents.

--- config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace config {
-	const std::string debug_top_directory = "./INTERNAL/";
-	const std::string input_top_directory = "./INPUT/";
-	const std::string output_top_directory = "./OUTPUT/";
-}
