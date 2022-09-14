d9f1e8c2f0e02cc34cde90911e51d63a16508b6e
Max <max.franck@iki.fi>
2022-09-14 18:29:00 +0300

    vertex_connectivity.h was the wrong version

--- include/vertex_connectivity.h
+++ include/vertex_connectivity.h
@@ -2,14 +2,15 @@
 
 #include "resettable_graph.h"
 #include "sparsify.h"
+#include "util.h"
 
 namespace vertex_connectivity {
 	// is residual graph SG k-connected near x?
 	void localec(resettable_graph& SG, vertex x, size_t v, size_t k, bool& found_cut);
 	// Does G have an unbalanced vertex cut smaller than k?
-	void unbalancedvc(const adjacency_list& G, resettable_graph& SG, size_t k, size_t min_vol, size_t max_vol, double sample_size_multiplier, vertex_set& cut, bool& found_cut);
+	void unbalancedvc(const adjacency_list& G, resettable_graph& SG, edge_sampler& sampler, size_t k, size_t min_vol, size_t max_vol, double sample_size_multiplier, vertex_set& cut, bool& found_cut);
 	// Does G have a balanced vertex cut smaller than k?
-	void balancedvc(const adjacency_list& G, resettable_graph& SG, size_t k, size_t repetitions, vertex_set& cut, bool& found_cut);
+	void balancedvc(const adjacency_list& G, resettable_graph& SG, edge_sampler& sampler, size_t k, size_t repetitions, vertex_set& cut, bool& found_cut);
 
 	// Maximum flow from vertex x to vertex y, up to a maximum value k.
 	size_t maxflow(resettable_graph& SG, vertex x, vertex y, size_t k);
